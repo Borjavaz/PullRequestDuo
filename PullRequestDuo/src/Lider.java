2c99ad9e93e044c7ffeecb4f8229514bcb8d50cf
Borja De Saa <bdesaavazquez@danielcastelao.org>
2025-01-30 12:31:48 +0100

    Lider Final

--- PullRequestDuo/src/Lider.java
+++ PullRequestDuo/src/Lider.java
@@ -3,5 +3,6 @@ public class Lider {
         System.out.println("Me llamo Borja y soy el lider del proyecto");
         System.out.println("Commit 1");
         System.out.println("Commit 2");
+        System.out.println("Commit Final");
     }
 }
