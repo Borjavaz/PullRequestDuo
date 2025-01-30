deed9238cb960cd379d90b49d160ebb59e9b3a77
Manuel Carrera Pazó <mcarrerapazo@danielcastelao.org>
2025-01-30 12:31:47 +0100

    Commit 1

---
+++ PullRequestDuo/src/colab.java
@@ -0,0 +1,6 @@
+public class colab {
+    public static void main(String[] args) {
+        System.out.println("Soy el colaborador (Manuel Carrera) del lider (Borja de Saá)");
+        System.out.println("Commit 1");
+    }
+}
